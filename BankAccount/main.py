5a9c1e4b597ab8e82bd1926236d9acb0d94d0ae2
Caimen French <cfrench8@charlotte.edu>
2026-02-12 20:42:41 -0500

    feat: Added second instance of CheckingAccount to test in main method

--- BankAccount/main.py
+++ BankAccount/main.py
@@ -7,13 +7,16 @@ person100 = SavingsAccount("Smith", 100, 10, 0.05, 790, 124)
 
 # Caiman's instance
 person2 = CheckingAccount("Caimen", 4000, 10, 420, 985)
+person200 = CheckingAccount("Guy", 3200, 10, 360, 986)
 
 # Test scenario User opening checking account and withdraw
 # Scenario 1: Under Limit
 person2.transfer_with_limit(1000)
+person2.transfer_with_limit(16)
 print("----------------------------")
 #Scenario 2: Over Limit
 person2.transfer_with_limit(2300)
+person200.transfer_with_limit(3200)
 print("----------------------------")
 
 # Testing SavingsAccount interest feature
