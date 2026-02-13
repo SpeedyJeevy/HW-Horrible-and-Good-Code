6c9ffbbc48384ee2caf464bdd52b424079634d21
Andy <andydchoo@gmail.com>
2026-02-12 19:37:02 -0500

    feat: adding savingsAccount to own file

---
+++ BankAccount/SavingsAccount.py
@@ -0,0 +1,12 @@
+# Savings Account with interest - Andy Choo
+import BankAccount
+
+# Savings Account subclass with interest rate
+class SavingsAccount(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.interest_rate = interest_rate
+
+    def add_interest(self):
+        interest = self.current_balance * self.interest_rate
+        self.current_balance += interest
