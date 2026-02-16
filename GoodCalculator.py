a58a4ddadd03115df43070f94794caee3b316dd0
Andy <andydchoo@gmail.com>
2026-02-16 16:25:24 -0500

    added documentation and comments for best practice

--- GoodCalculator.py
+++ GoodCalculator.py
@@ -1,22 +1,25 @@
+# Taking input for the operation, and two numbers needed for computing
 operations = input("Please select one of the arithmetic operations: + , - , * , or / ")
 
 num1 = float(input("Please enter your first number: "))
 
 num2 = float(input("Please enter your second number: "))
 
+# Adding num1 and num2 then saving to calculation
 if operations == "+":
     calculation = num1 + num2
 
-
+# Subtracting num1 and num2 then saving to calculation
 elif operations == "-":
     calculation = num1 - num2
 
-
+# Multiplying num1 and num2 then saving to calculation
 elif operations == "*":
     calculation = num1 * num2
 
-
+# Dividing num1 and num2 then saving to calculation
 elif operations == "/":
     calculation = num1 / num2
 
+# Printing the calculated result
 print(calculation)
